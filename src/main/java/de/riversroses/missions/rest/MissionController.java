f7df9ba75511ae854b5e496b613b4f64703776c6
Florian Babel <f.babel2000@gmail.com>
2026-02-10 19:40:07 +0100

    add solution for task 1 (endpoints)

--- src/main/java/de/riversroses/missions/rest/MissionController.java
+++ src/main/java/de/riversroses/missions/rest/MissionController.java
@@ -1,5 +1,35 @@
 package de.riversroses.missions.rest;
 
+import de.riversroses.missions.business.MissionService;
+import de.riversroses.missions.dto.MissionCompletionDto;
+import de.riversroses.missions.dto.MissionPayloadDto;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Body;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
+
+@Controller("/missions")
 public class MissionController {
+
+    private final MissionService missionService;
+
+    public MissionController() {
+        missionService = new MissionService();
+    }
+
+    @Get
+    public MissionPayloadDto getMission() {
+        return missionService.generateOrReuseMission();
+    }
+
+    @Post("/complete")
+    public HttpResponse<?> completeMission(@Body MissionCompletionDto mission) {
+        if (mission.reward <= 0) {
+            return HttpResponse.badRequest();
+        }
+        missionService.markCompleted(mission);
+        return HttpResponse.ok();
+    }
 
 }
