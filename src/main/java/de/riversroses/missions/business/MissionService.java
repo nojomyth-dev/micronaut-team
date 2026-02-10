7c27b143b840282b1b793b03b6abf100bcd411d4
Florian Babel <f.babel2000@gmail.com>
2026-02-10 20:33:23 +0100

    remove config

--- src/main/java/de/riversroses/missions/business/MissionService.java
+++ src/main/java/de/riversroses/missions/business/MissionService.java
@@ -5,7 +5,6 @@ import de.riversroses.missions.dto.MissionCompletionDto;
 import de.riversroses.missions.dto.MissionPayloadDto;
 import de.riversroses.missions.model.MissionLog;
 import de.riversroses.missions.model.MissionStatus;
-import io.micronaut.context.annotation.Value;
 import jakarta.inject.Singleton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,11 +20,9 @@ public class MissionService {
     private final MissionLogRepository repo;
     private final MissionRng rng;
 
-    @Value("${game.world.width:1000}")
-    protected double worldWidth;
+    protected double worldWidth = 1000;
 
-    @Value("${game.world.height:1000}")
-    protected double worldHeight;
+    protected double worldHeight = 1000;
 
     public MissionService(MissionLogRepository repo, MissionRng rng) {
         this.repo = repo;
