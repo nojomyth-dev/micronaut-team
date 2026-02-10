5d2087b97de118ad2786ed3d6e3aea439f783e29
Florian Babel <f.babel2000@gmail.com>
2026-02-10 18:42:54 +0100

    remove validation and serdable at MissionCompletionDto

--- src/main/java/de/riversroses/missions/dto/MissionCompletionDto.java
+++ src/main/java/de/riversroses/missions/dto/MissionCompletionDto.java
@@ -1,22 +1,12 @@
 package de.riversroses.missions.dto;
-import io.micronaut.serde.annotation.Serdeable;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
 
-@Serdeable
-@Data
 public class MissionCompletionDto {
 
-  @NotBlank
   public String missionId;
   
-  @NotBlank
   public String shipId;
   
-  @NotBlank
   public String teamId;
 
-  @Min(0)
   public Integer reward;
 }
