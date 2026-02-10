cb9dac1804efdf2a620fe9c5b6a0537bd0698925
Florian Babel <f.babel2000@gmail.com>
2026-02-10 19:31:32 +0100

    remove annotations from missionpayload

--- src/main/java/de/riversroses/missions/dto/MissionPayloadDto.java
+++ src/main/java/de/riversroses/missions/dto/MissionPayloadDto.java
@@ -1,20 +1,12 @@
 package de.riversroses.missions.dto;
-import io.micronaut.serde.annotation.Serdeable;
-import jakarta.validation.constraints.NotNull;
-import lombok.Data;
 
-@Serdeable
-@Data
 public class MissionPayloadDto {
 
   public String id;
   
-  @NotNull
   public String description;
   
-  @NotNull
   public Double x;
   
-  @NotNull
   public Double y;
 }
