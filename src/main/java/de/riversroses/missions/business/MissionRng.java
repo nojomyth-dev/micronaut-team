2f0214f8d14e516bfc71ef7b85783b2ce19433e1
Florian Babel <f.babel2000@gmail.com>
2026-02-10 20:28:30 +0100

    remove annotation before

--- src/main/java/de/riversroses/missions/business/MissionRng.java
+++ src/main/java/de/riversroses/missions/business/MissionRng.java
@@ -1,10 +1,7 @@
 package de.riversroses.missions.business;
 
-import io.micronaut.context.annotation.Prototype;
-
 import java.util.Random;
 
-@Prototype
 public class MissionRng {
     private final Random random = new Random(11387174);
 
