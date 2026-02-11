8d7647cdfb5291096f3d37224677d3d29281ce7a
314wi <falko.piwinger@gmail.com>
2026-02-11 16:51:00 +0100

    Aufgabe 6 musterlösung

--- src/main/java/de/riversroses/missions/rest/MissionController.java
+++ src/main/java/de/riversroses/missions/rest/MissionController.java
@@ -5,7 +5,10 @@ import de.riversroses.missions.business.MissionService;
 import de.riversroses.missions.dto.MissionCompletionDto;
 import de.riversroses.missions.dto.MissionPayloadDto;
 import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
 import io.micronaut.http.annotation.*;
+import io.micronaut.http.annotation.Error;
+import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Valid;
 import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
@@ -31,5 +34,11 @@ public class MissionController {
     public HttpResponse<?> complete(@Body @Valid MissionCompletionDto completion) {
         missionService.markCompleted(completion);
         return HttpResponse.ok();
+    }
+
+    @Error(exception = ConstraintViolationException.class)
+    public HttpResponse<?> handleValidationErrors() {
+        return HttpResponse.status(HttpStatus.I_AM_A_TEAPOT)
+                .body("I'm a teapot. Stop sending junk missions!");
     }
 }
