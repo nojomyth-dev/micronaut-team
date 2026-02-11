b24c8ff09db7ffe966f0bdc6457bbb868c325aab
nojomyth-dev <cyrille0raven@gmail.com>
2026-02-11 21:53:00 +0100

    Fix code

--- src/main/java/de/riversroses/infra/client/GameServerClientFallback.java
+++ src/main/java/de/riversroses/infra/client/GameServerClientFallback.java
@@ -8,16 +8,12 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.Collections;
 
-@Fallback
-@Slf4j
 public class GameServerClientFallback {
 
   public void setCourse(String token, SetCourseRequestDto req) {
-    log.warn("Fallback: Could not set course for ship {}", req.shipId());
   }
 
   public RadarScanResponseDto scan(String token, @Nullable String shipId) {
-    log.warn("Fallback: Could not scan from ship {}", shipId);
-    return new RadarScanResponseDto(Collections.emptyList(), Collections.emptyList());
+    return null;
   }
 }
