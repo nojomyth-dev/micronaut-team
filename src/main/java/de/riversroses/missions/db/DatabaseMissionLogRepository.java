4941d606b8fddfabcb7680d3f82966818ddb07af
nojomyth-dev <cyrille0raven@gmail.com>
2026-02-11 18:44:19 +0100

    Fixing hardcoded repo to interface

--- src/main/java/de/riversroses/missions/db/DatabaseMissionLogRepository.java
+++ src/main/java/de/riversroses/missions/db/DatabaseMissionLogRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Singleton
-public class DatabaseMissionLogRepository implements MissionLogRepository{
+public class DatabaseMissionLogRepository implements MissionLogRepository {
     private final JpaMissionLogRepository jpaMissionLogRepository;
 
     public DatabaseMissionLogRepository(JpaMissionLogRepository jpaMissionLogRepository) {
